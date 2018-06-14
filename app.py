f006656ecfeec85fc86e705c2236b26ecff6c223
halfnibble <josh@halfnibble.com>
2018-06-13 20:33:47 -0700

    Trying cursor last insert again.

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def insert_todo(created_by=None, name=None, location=None):
         """
     cursor.execute(query, (created_by, name, location))
     connection.commit()
-    new_id = connection.insert_id()
+    new_id = cursor.lastrowid
     if new_id:
         return True
     else:
